18f779f1e253d7c4f2d779ad157348325d45dc98
IqraAfzaalM <97847537+IqraAfzaalM@users.noreply.github.com>
2022-06-30 23:51:31 +0500

    Add files via upload

---
+++ main/java/com/example/oelfinal/Register.java
@@ -0,0 +1,94 @@
+package com.example.oelfinal;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.nfc.Tag;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ProgressBar;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.material.tabs.TabLayout;
+import com.google.firebase.FirebaseException;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.PhoneAuthCredential;
+import com.google.firebase.auth.PhoneAuthProvider;
+
+import java.util.concurrent.TimeUnit;
+
+public class Register extends AppCompatActivity {
+FirebaseAuth fAuth;
+EditText phoneNumber, codeEnter;
+Button nextBtn;
+ProgressBar progressBar;
+TextView state;
+String verificationId;
+    PhoneAuthProvider.ForceResendingToken token;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_register);
+
+        fAuth = FirebaseAuth.getInstance();
+        phoneNumber = findViewById(R.id.phone);
+        codeEnter = findViewById(R.id.codeEnter);
+        progressBar = findViewById(R.id.progressBar);
+        nextBtn = findViewById(R.id.nextBtn);
+        state = findViewById(R.id.state);
+
+
+        nextBtn.setOnClickListener(new OnClickListener(){
+
+            @Override
+            public void onClick(View view) {
+                if(!phoneNumber.getText().toString().isEmpty() && phoneNumber.getText().toString().length()==15){
+                    String phoneNum = "+1"+phoneNumber.getText().toString();
+                    progressBar.setVisibility(View.VISIBLE);
+                    state.setText("Sending OTP...");
+                    state.setVisibility(View.VISIBLE);
+requestOTP(phoneNum);
+
+                }else{
+                    phoneNumber.setError("Phone no is not valid");
+                }
+            }
+        });
+
+
+    }
+
+    private void requestOTP(String phoneNum) {
+        PhoneAuthProvider.getInstance().verifyPhoneNumber(phoneNum, 60L, TimeUnit.SECONDS, this, new PhoneAuthProvider.OnVerificationStateChangedCallbacks() {
+            @Override
+            public void onCodeSent(@NonNull String s, @NonNull PhoneAuthProvider.ForceResendingToken forceResendingToken) {
+                super.onCodeSent(s, forceResendingToken);
+                progressBar.setVisibility(View.GONE);
+                state.setVisibility(View.GONE);
+                codeEnter.setVisibility(View.VISIBLE);
+                verificationId =s;
+                token = forceResendingToken;
+            }
+
+            @Override
+            public void onCodeAutoRetrievalTimeOut(@NonNull String s) {
+                super.onCodeAutoRetrievalTimeOut(s);
+            }
+
+            @Override
+            public void onVerificationCompleted(@NonNull PhoneAuthCredential phoneAuthCredential) {
+
+            }
+
+            @Override
+            public void onVerificationFailed(@NonNull FirebaseException e) {
+                Toast.makeText(Register.this," Cannot create account"+ e.getMessage(), Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
+}
